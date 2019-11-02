6dce125cfa8317cfb709471f9e44a3a9ebc32c14
“krishnaSoni2113” <pathik@mindinventory.com>
2019-11-02 17:19:40 +0530

    - Initial commit

---
+++ BirthdayDates/BirthdayDates.h
@@ -0,0 +1,19 @@
+//
+//  BirthdayDates.h
+//  BirthdayDates
+//
+//  Created by mac-0005 on 02/11/19.
+//  Copyright © 2019 mac-0005. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BirthdayDates.
+FOUNDATION_EXPORT double BirthdayDatesVersionNumber;
+
+//! Project version string for BirthdayDates.
+FOUNDATION_EXPORT const unsigned char BirthdayDatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BirthdayDates/PublicHeader.h>
+
+
---
+++ BirthdayDates/BirthdayDates.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayDates.swift
+//  BirthdayDates
+//
+//  Created by mac-0005 on 02/11/19.
+//  Copyright © 2019 mac-0005. All rights reserved.
+//
+
+import Foundation
+
+open class BirthdayDates: NSObject {
+    
+    open  func getBirthDates() -> String {
+        
+        return "21 march"
+    }
+}
